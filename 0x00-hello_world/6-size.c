7e7a5b872255a6de34ad23cad89498e19672b66a
Your Name <you@example.com>
2021-08-26 13:39:02 -0700

    printing size of c types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char:%lu"sizeof(char));
-printf("Size of an int:%lu"sizeof(int));
-printf("Size of a long int:%lu"sizeof(long int));
-printf("Size of a long long int:%lu"sizeof(long long int));
-printf("Size of a float:%lu"sizeof(float));
+printf("Size of a char:%lu", sizeof(char));
+printf("Size of an int:%lu", sizeof(int));
+printf("Size of a long int:%lu", sizeof(long int));
+printf("Size of a long long int:%lu", sizeof(long long int));
+printf("Size of a float:%lu", sizeof(float));
 return (0);
 }
