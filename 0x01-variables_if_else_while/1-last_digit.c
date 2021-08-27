9c0bc8ac24b8f2c02197c2e32df93472e8afb3e9
Your Name <you@example.com>
2021-08-27 09:00:26 -0700

    checking the characteristics of the last digit of a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ p = n % 10;
 if (p > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, p);
 else if (p < 6 && p != 0)
-printf("Last digit of %d is %d and and is less than 6 and not 0\n", n, p);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
 else
 printf("Last digit of %d is %d and is 0\n", n, p);
 return (0);
