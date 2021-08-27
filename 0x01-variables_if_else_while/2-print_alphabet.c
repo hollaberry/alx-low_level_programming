e0a00cb01c2391a0bc4d738776f8a83a010487f7
Your Name <you@example.com>
2021-08-27 09:23:53 -0700

    program to print alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- To print the alphabets in lower case
+ * ch is assigned a value of a and is used to control the iteration
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+return (0);
+}
