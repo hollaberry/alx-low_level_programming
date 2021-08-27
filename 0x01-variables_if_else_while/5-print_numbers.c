f0cc1616ea1c85cec6d1a38259fa1ab59b32d564
Your Name <you@example.com>
2021-08-27 14:57:43 -0700

    The single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - To print the single digits of base 10
+ * For loop will run the iteration using a declared variable
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("The single digit numbers of base 10 are %d\n", i);
+return (0);
+}
