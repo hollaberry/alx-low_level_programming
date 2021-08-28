14000b11f7364cb8ab59887242e11e2d34a09d4b
Your Name <you@example.com>
2021-08-28 05:35:31 -0700

    combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - To print all combinations of single digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for  (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
