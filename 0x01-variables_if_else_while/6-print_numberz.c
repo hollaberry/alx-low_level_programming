8261c8aa67c140cd152a2f5879221e2adafa7a9e
Your Name <you@example.com>
2021-08-28 03:09:15 -0700

    using putchar only to print single digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - To print the single digits of base 10
+ * main - To print the single digits of base 10 using only putchar
  * For loop will run the iteration using a declared variable
  * Return: Always 0 (success)
  */
 int main(void)
 {
 int i = 0;
-while (i < 9)
+for (i = 0; i <= 9; i++)
 {
-putchar(i);
+putchar(i + '0');
 }
 putchar('\n');
 return (0);
