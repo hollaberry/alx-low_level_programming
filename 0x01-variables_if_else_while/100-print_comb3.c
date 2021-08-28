69d6634b2fdd2215bcfca730d399661fe4e23f72
Your Name <you@example.com>
2021-08-28 13:43:57 -0700

    double digits using putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,15 @@ for (i = 0; i <= 89; i++)
 {
 n = i / 10;
 p = i % 10;
-if (p == 0 || p == n)
-continue;
+if (n < p)
+{
 putchar(n + '0');
 putchar(p + '0');
+}
+else
+{
+continue;
+}
 if (i < 89)
 {
 putchar(',');
