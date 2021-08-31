2077f69cb8d388ce2cc631bd9d9abc4a813bdbb7
Your Name <you@example.com>
2021-08-31 09:11:55 -0700

    Using functions for putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main- Prototype of function declaration
+ *
+ * Return: Always ch (success)
+ */
+/**
+ * main - To print Holberton using _putchar function
+ *
+ * Return: Always ch (success)
+ */
+int _putchar(int ch);
+int main(void)
+{
+_putchar(72);
+_putchar(111);
+_putchar(108);
+_putchar(98);
+_putchar(101);
+_putchar(114);
+_putchar(116);
+_putchar(111);
+_putchar(110);
+_putchar('\n');
+return (0);
+}
+/**
+ * main- function to print char ch as argument
+ *
+ * Return Always ch (success)
+ */
+int _putchar(int ch)
+{
+int ch;
+putchar(ch);
+return (ch);
+}
