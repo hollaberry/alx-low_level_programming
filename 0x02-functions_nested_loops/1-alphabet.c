d54f636dec3d8671c6c2108c3853b850f9ae6ca0
Your Name <you@example.com>
2021-08-31 13:14:11 -0700

    Using a void function to print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+/**
+ * main- Prototype of function declaration
+ *
+ * Return: Always ch (success)
+ */
+int main(void)
+{
+print_alphabet();
+putchar('\n');
+return (0);
+}
