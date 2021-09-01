053f12df5657e715b5391df0a5d53bf690bd3083
Your Name <you@example.com>
2021-09-01 07:20:28 -0700

    function to print the last digit of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit -function to check the absolute value of an integer
+ * @c:This is the integer whose last digit needs to be picked up
+ * Return: Always the abs value of the number
+ */
+int print_last_digit(int c)
+{
+c = c % 10;
+return (c);
+}
