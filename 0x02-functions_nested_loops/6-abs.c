4cd981a600f6ec05fac0b19bcfea87bb50128a62
Your Name <you@example.com>
2021-09-01 05:50:48 -0700

    function to print abs value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * _abs -function to check the absolute value of an integer
- * @c: This is the integer to be check for its absolute value
+ * @int: This is the integer to be check for its absolute value
  * Return: Always the abs value of the number
  */
-int _abs(int c)
+int _abs(int)
 {
-return (abs c);
+c = abs(int);
+return (c);
 }
