f90236f8410c55ff7b446fc898c19ce2ecf2d138
Your Name <you@example.com>
2021-09-01 14:47:51 -0700

    printing all the sec min and hour in a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,18 +4,18 @@
  */
 void jack_bauer(void)
 {
-int i, j;
+int i = 0, j;
 while (i < 24)
 {
 j = 0;
 while (j < 60)
 {
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(':');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-putchar('\n');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
 }
 j++;
 }
