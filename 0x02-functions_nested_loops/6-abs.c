4adf5cc9fbedf37feff596b690c8c879b66b4a34
Your Name <you@example.com>
2021-09-01 05:32:52 -0700

    printing the absolute value of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * @c: This is the integer to be check for its absolute value
  * Return: Always the abs value of the number
  */
-int _abs(int c)
+int _abs(int)
 {
-return (abs(c));
+return (abs());
 }
