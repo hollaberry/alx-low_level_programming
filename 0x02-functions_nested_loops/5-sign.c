cbb03d11b19b95c7f55289b402a7b562b19ea58d
Your Name <you@example.com>
2021-09-01 04:21:01 -0700

    A function to ckeck the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number
+ * @n: This is the integer to be checked
+ * Return: Always 1, 0, and -1 if n > 0, n < 0, and n = 0 respectively
+ */
+int print_sign(int n)
+{
+if (n > 0)
+_putchar(43);
+return (1);
+else if (n == 0)
+_putchar(48);
+return (0);
+else
+_putchar('-' + 49);
+return (-1);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c~
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha -function to check if a character is a lower case alphabet
+ * @c: This is the character to be checked
+ * Return: Always 1 if character is an alphabet and 0 if otherwise
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
