1547b1c7fceb5c10b82bf00c25e34afb13f79b0d
Your Name <you@example.com>
2021-09-01 06:47:17 -0700

    a function to output the abs of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,13 @@
 /**
  * _abs -function to check the absolute value of an integer
  * @c: This is the integer to be check for its absolute value
- * @r: another int to hold negative value
  * Return: Always the abs value of the number
  */
 int _abs(int c)
 {
-int r = -1;
 if (c < 0)
 {
-c = (r * c);
+c = (c * -1);
 }
 return (c);
 }
