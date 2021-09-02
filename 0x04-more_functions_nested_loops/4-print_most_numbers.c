6bd2b1b0170fe4b55774710e2aa5d9ecc2e2f6fb
Your Name <you@example.com>
2021-09-02 15:22:17 -0700

    function to print 0-9 with exceptions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers -function to print numbers from 0 to 9
+ * Return: Always 1 (success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if (i != 50 || i != 52)
+_putchar(i);
+}
+_putchar('\n');
+}
