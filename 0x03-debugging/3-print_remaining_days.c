6a0a27ecf22682299a9ed8088e5ad63f343e40d8
Your Name <you@example.com>
2021-09-02 12:42:56 -0700

    last task of debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,12 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if ((month == 2 && day <= 58) || ((month == 2 && day == 60) && (year % 4 != 0 || year % 400 != 0)))
+if ((month == 2 && day == 60) && (year % 4 != 0 || year % 400 != 0))
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else if
+(month == 2 && day <= 58)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
