403e57787d025b9feaff6ec8c654c426cb79b211
Your Name <you@example.com>
2021-09-02 14:45:49 -0700

    function to check if a character is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit -function to check if a character is a digit
+ * @c: This is the integer to be check if its a digit
+ * Return: Always 1 (success)
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
