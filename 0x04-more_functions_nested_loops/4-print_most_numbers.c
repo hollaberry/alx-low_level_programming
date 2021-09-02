c95d0b71996de13cc56b52f066e8264f33c32c17
Your Name <you@example.com>
2021-09-02 15:59:42 -0700

    function to print 0-9 with exeption

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers -function to print numbers from 0 to 9
+ * print_most_numbers -function to print numbers from 0 to 9 except 2 and 4
  * Return: Always 1 (success)
  */
 void print_most_numbers(void)
