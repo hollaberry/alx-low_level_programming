cb989a82659e5692a73716d7a5052fe493ea7f12
Your Name <you@example.com>
2021-09-02 15:05:50 -0700

    functions to print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers -function to print numbers from 0 to 9
+ * @i: This is the integer to be printed
+ * Return: Always 1 (success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c~
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul -function to multiply two integers
+ * @a: This is the first integer
+ * @b: This is the second integer
+ * Return: Always 1 (success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
