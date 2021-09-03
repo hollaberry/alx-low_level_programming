79ec1d2b9686faee882846ad7e5801f53bf92619
Your Name <you@example.com>
2021-09-03 12:38:18 -0700

    function to print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - function to print square of a given integer
+ * @size: function argument to determine square properties
+ * Return: Always 0 (success)
+ */
+void print_square(int size)
+{
+int i, j;
+for (i = 0; i <= size; i++)
+{
+for (j = 0; j <= size; j++)
+{
+_putchar('#')
+}
+
+_putchar('\n');
+}
+}
