41d948a0c6a6a7d5a5e51239cdb1216f5d8fce4d
Your Name <you@example.com>
2021-09-03 14:11:14 -0700

    function to print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,16 @@ void print_diagonal(int n)
 {
 int i, j;
 
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 0; j <= n; j++)
+for (j = 0; j < n; j++)
 {
 
 if (j == i)
-
-
 _putchar(92);
 
-
-else
-
-
+else if (i > j)
 _putchar(' ');
-
-
 }
 _putchar('\n');
 }
