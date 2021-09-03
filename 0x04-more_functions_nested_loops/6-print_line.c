b8bf0fafba4778706008cb75911620e2d37b23d4
Your Name <you@example.com>
2021-09-03 07:16:40 -0700

    function to print a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line -function to print a straight line
+ * @n: integer is value is to be use to determine length of straight line
+ * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+int i = 1;
+while (i <= n)
+{
+
+_putchar(95);
+i++;
+
+}
+
+_putchar('\n');
+
+}
