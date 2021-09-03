a20ddbed18a529d10d79929068e7792c76e768af
Your Name <you@example.com>
2021-09-03 01:30:01 -0700

    function to print 0-9 with exceptions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,11 @@
 void print_most_numbers(void)
 {
 int i;
-for (i = 48; i <= 57; i++)
+for (i = 0; i <= 57; i++)
 {
-if (i != 50 || i != 52)
+if (i != 50 && i != 52)
 {
 _putchar(i);
-}
-else
-{
-continue;
 }
 }
 _putchar('\n');
