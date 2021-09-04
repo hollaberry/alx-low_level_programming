7d4371b0525952e9ccd0e91e1f0201b326148fd8
Your Name <you@example.com>
2021-09-04 00:15:27 -0700

    fizzbuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && !(i % 5 == 0) && !(i % 15 == 0))
 {
-printf("Fizz");
+printf(" Fizz");
 }
 else if ((i % 5 == 0) && !(i % 3 == 0))
 {
-printf("Buzz");
+printf(" Buzz");
 }
 else if (i % 15 == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
 else if (i == 1)
 {
@@ -28,7 +28,7 @@ printf("%d", i);
 }
 else
 {
-printf("%d", i);
+printf(" %d", i);
 }
 printf(" ");
 }
