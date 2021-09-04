d6830be3452dfee5d1e3f01177c7d7fe97f9dce4
Your Name <you@example.com>
2021-09-04 08:10:51 -0700

    function to print triangle shape

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - function to print triangle of a given integer
+ * @size: function argument to determine triangle properties
+ * Return: Always 0 (success)
+ */
+void print_triangle(int size)
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = size; j >= 1 ; j--)
+{
+if (j > i)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
