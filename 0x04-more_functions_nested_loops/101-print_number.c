a997a4aab1c9d447762a80dd8cde402714711cee
Your Name <you@example.com>
2021-09-04 10:22:47 -0700

    function to print a number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_number - function to print numbers
- * @n: variable argument which house to number to be printed
+ * @n: variable argument which house the number to be printed
  */
 void print_number(int n)
 {
@@ -15,5 +15,4 @@ if (n > 9)
 print_number(n / 10);
 }
 _putchar((n % 10) + '0');
-
 }
