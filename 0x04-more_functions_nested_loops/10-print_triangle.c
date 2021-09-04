8f01c6e241443cf6d8c9b23cd11b61886366ec7b
Your Name <you@example.com>
2021-09-04 08:36:41 -0700

    function to print a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,24 @@
 #include "main.h"
 /**
- * print_triangle - function to print triangle of a given integer
- * @size: function argument to determine triangle properties
+ * print_triangle - function to print square of a given integer
+ * @size: function argument to determine square properties
  * Return: Always 0 (success)
  */
 void print_triangle(int size)
 {
 int i, j;
-for (i = 1;  i <= size; i++)
+for (i = 1; i <= size; i++)
 {
-for (j = size; j >= 1; j--)
+for (j = size; j >= 1; j++)
 {
 if (j > i)
-{
-putchar(' ');
-}
+_putchar(' ');
 else
-{
 _putchar('#');
 }
-}
+
 _putchar('\n');
+
 }
 if (size <= 0)
 {
