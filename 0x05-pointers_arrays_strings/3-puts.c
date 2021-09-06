08f0d19b644f88e6b689dda61e531c301b317884
Your Name <you@example.com>
2021-09-06 13:57:55 -0700

    function to print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,20 +6,10 @@
  * Return: Always str (success)
  */
 
-int _strlen(char *s)
-{
-int count = 0;
-while (*s != '\0')
-{
-count++;
-s++;
-}
-return (count);
-}
-
 void _puts(char *str)
 {
 int i;
 i = _strlen(str);
 write(1, str, i);
+_putchar('\n');
 }
