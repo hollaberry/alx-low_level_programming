5f37286495b47609c6d10e6ea096173ae2de3f51
Your Name <you@example.com>
2021-09-06 06:32:58 -0700

    pointer first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - function to change the value of a variable using a pointer
- * @*n: pointet to be assigned  new value 98
+ * @n:pointer to be assigned new value 98
  * Return Always (0) (success)
  */
 void reset_to_98(int *n)
