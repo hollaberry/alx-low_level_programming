99109cd7a79121aaa00d3401cdcd6f2ae306e245
Your Name <you@example.com>
2021-09-07 09:29:34 -0700

    function to output everyother character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - a function to print every other character of a string
+ * @str:pointer pointing to the string characters
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+int len = 0;
+int i = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+len = len - 1;
+
+char p[len];
+for (i = 0; i < len; i + 2)
+{
+_putchar(s[i]);
+p[i / 2] = s[i];
+}
+}
