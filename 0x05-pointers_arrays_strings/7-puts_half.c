6f4000bcfef9b1be6bd23a3718f6e62695c7f348
Your Name <you@example.com>
2021-09-07 13:53:04 -0700

    function to pint half part of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,13 @@ void puts_half(char *str)
 {
 int len = 0;
 
-while (str[len] != '\0')
-{
-len++;
-}
+ for (len = 0; str[len] != '\0'; i++)
+   ;
 
-for (len /= 2; str[len]!= '\0'; len++)
+len++;
+
+
+for (len /= 2; str[len] != '\0'; len++)
 {
 _putchar(str[len]);
 }
