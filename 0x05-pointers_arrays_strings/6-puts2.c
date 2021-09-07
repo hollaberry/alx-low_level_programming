16a4b81460c0151a8a383b82bc4714cd44a27b84
Your Name <you@example.com>
2021-09-07 09:37:47 -0700

    function to print everyother character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,16 +9,16 @@ void puts2(char *str)
 {
 int len = 0;
 int i = 0;
+char p[i];
 while (str[len] != '\0')
 {
 len++;
 }
 len = len - 1;
 
-char p[len];
 for (i = 0; i < len; i + 2)
 {
-_putchar(s[i]);
-p[i / 2] = s[i];
+_putchar(str[i]);
+p[i / 2] = str[i];
 }
 }
