3e2ba051874bfaea8859b67cb913a621e2a33326
Your Name <you@example.com>
2021-09-09 07:14:16 -0700

    function to concatenate two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
+int j, i;
 for (j = 0; dest[j] != '\0'; j++)
-{
+
 ;
-}
+
 for (i = 0; i < n && src[i] != '\0'; i++)
-{
 dest[j + i] = src[i];
-}
+
+
 dest[j + i] = '\0';
 return (dest);
 }
