60ec464dbbb04e1ca09ebb410a438b8fcb96ab07
Your Name <you@example.com>
2021-09-10 07:54:41 -0700

    function to replace specific memebre of a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - function to change specific members of a string
+ * @str:pointer to the string
+ * Return: Always (str)
+ */
+char *leet(char *str)
+{
+int i, k, j;
+char sep[] = "aAeEoOtTlL";
+char pes[] = "4433007711";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0, k = 0; j < 10; j++, k++)
+{
+if (str[i] == sep[j])
+{
+str[i] = pes[k];
+break;
+}
+return (str);
+}
