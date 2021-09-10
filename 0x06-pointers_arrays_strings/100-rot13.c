5bbe2b39609882603ea808b45360ce83b24e7257
Your Name <you@example.com>
2021-09-10 09:20:09 -0700

    function to encode in rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - funtion to encode in rot13
+ * @str:pointer to the string
+ * Return: Always (str)
+ */
+char *rot13(char *str)
+{
+int i, j;
+char sep[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char pes[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0, k = 0; j < 52; j++, k++)
+{
+if (str[i] == sep[j])
+{
+str[i] = pes[k];
+break;
+}
+}
+}
+return (str);
+}
