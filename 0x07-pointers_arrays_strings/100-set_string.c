f216dc8e6744958727c7f16d878daf166d5467a8
Your Name <you@example.com>
2021-09-14 06:05:46 -0700

    function that set the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - function to print a chess board
+ * @s: pointer to pointer base string
+ * @to: pointer 2
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
