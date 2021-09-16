290f777143df57b902dc8e68b5681193922e6857
Your Name <you@example.com>
2021-09-16 07:18:35 -0700

    function to check if a number is a prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,6 @@ void _print_rev_recursion(char *s);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int val);
+int check_prime(int n, int resp);
+int is_prime_number(int n);
 #endif
