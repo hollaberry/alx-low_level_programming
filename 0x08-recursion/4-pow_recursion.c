abec14878ad97418ea5739ae19253cb4fc3314c3
Your Name <you@example.com>
2021-09-16 05:59:31 -0700

    function to calculate the power of a number recursively

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,13 +11,13 @@ if (y < 0)
 {
 return (-1);
 }
-else if (y == 0)
-{
-return (1);
-}
 else if (y == 1)
 {
 return (x);
 }
-return (x * _power_recursion(x, y - 1));
+else if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
 }
