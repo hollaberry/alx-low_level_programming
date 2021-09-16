1a6a1b9d0efd99dfcecc338831e1415f0ec5c1fd
Your Name <you@example.com>
2021-09-16 06:40:18 -0700

    function to get the square root of a number recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: integer that need it square root found
+ * Return: Always square
+ */
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+
+/**
+ * square - function to determine the square root
+ * @n: Number whose square root is to be found
+ * @val: checker of true square root value
+ * Return: Always square
+ */
+int square(int n, int val)
+{
+if ((val * val) == n)
+{
+return (val);
+else if (val * val < n)
+return (square(n, val + 1));
+
+else
+return (-1);
+}
