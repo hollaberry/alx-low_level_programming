f8ffdff6d9163298ae16101ae27248599c1b1fc4
Your Name <you@example.com>
2021-09-18 14:06:17 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: an argument of main that house argument of program in an array format
  * Return: Always 0
  */
-int main(char argc, char *argv[])
+int main(int argc, char *argv[])
 {
-printf("%s\n", argv);
+printf("%s\n", argv[0]);
 }
