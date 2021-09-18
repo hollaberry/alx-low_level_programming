4f6c800d3517185d20461b40c3e0da5a65b29c1d
Your Name <you@example.com>
2021-09-18 14:41:11 -0700

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the number of arguments passed into it
+ * @argc: an argument of main that counts number of argument passed
+ * @argv: an argument of main that house argument of program in an array format
+ * Return: Always 0
+ */
+int main(int argc, char const *argv[])
+{
+(void)argv;
+int i;
+printf("%d\n", argc);
+return (0);
+}
