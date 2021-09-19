5eb963d92224b7b330964e7de19fad9d3eaf256a
Your Name <you@example.com>
2021-09-19 12:36:36 -0700

    program to print addition of integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,44 +2,33 @@
 #include <stdlib.h>
 
 /**
- * check - function to  check if array contain a non int
- * @s: pointer to string
- * Return: Always 0
- */
-int check(const char *s)
-
-{
-int i;
-while (s[i] != '\0')
-{
-if (s[i] < '0' || s[i] > '9')
-return (1);
-
-i++;
-
-}
-return (0);
-}
-
-/**
  * main - Program that prints the addition of  numbers
  * @argc: an argument of main that counts number of argument passed
  * @argv: an argument of main that house argument of program in an array format
  * Return: Always 0
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-int n = 0;
-while (--argc)
+int n = 0, k, j, l, i;
+for (i = 0; i < argc; i++)
 {
-if (check(argv[argc]))
+for (j = 0; argv[i][j] != '\0'; j++)
+if (argv[i][j] > '9' || argv[i][j] < '0')
 {
 printf("Error\n");
 return (1);
 }
-n += atoi(argv[argc]);
+}
 }
 
-printf("%i\n", n);
+for (k = 0; k < argc; k++)
+{
+l = atoi(argv[k]);
+if (l >= 0)
+{
+n += l;
+}
+}
+printf("%d\n", n);
 return (0);
 }
