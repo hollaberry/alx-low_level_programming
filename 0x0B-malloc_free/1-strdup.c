27a4bd9b5c11e8edb544f15cd8f642f7bff672c6
hollaberry <you@example.com>
2021-09-22 05:05:59 -0700

    function that return a pointer to a newly created memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function that return a pointer to a newly created space in memor
+ * @str: The size of char array
+ * Return: Always (p) or (NULL)
+ */
+char *_strdup(char *str)
+{
+int len, i, j;
+char *p;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+;
+}
+len  = i;
+p = malloc((len + 1) * sizeof(char);
+if (p == NULL)
+{
+return (NULL);
+}
+for (j = 0; j < len; j++)
+{
+p[j] = str[j];
+}
+p[j] = '\0';
+return (p);
+}
