ff210ab2b12238e12fe23ae77263a90bd8dd9462
hollaberry <you@example.com>
2021-09-27 09:30:55 -0700

    structure dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a define structure of a dog
+ * @name: character string
+ * @age: float
+ * @owner: character owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_u;
+#endif
