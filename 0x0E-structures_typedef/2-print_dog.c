455f9dfacb75eb16708558422acbf8007208b31e
hollaberry <you@example.com>
2021-09-27 10:30:59 -0700

    fix typo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ if (d != NULL)
 printf("Name: %s\nAge: %f\nOwner: %s\n",
 (*d).name != NULL ? d->name : "(nil)",
 (*d).age,
-(*d).owner != NULL ? d->Owner : "(nil)");
+(*d).owner != NULL ? d->owner : "(nil)");
 }
 }
