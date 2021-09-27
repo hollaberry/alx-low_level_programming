efeae2676170cdbd7152f055d4ea9c679f7411ff
hollaberry <you@example.com>
2021-09-27 08:54:20 -0700

    fix the main argument

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,7 @@
  * main - program that prints the name of the file it is compiled from
  * Return: Always (0)
  */
-int main(int argc, const char *argv[])
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
