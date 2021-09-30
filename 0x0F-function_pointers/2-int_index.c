a3f73950fd55ee081940d7f7b9f197b47694dc83
hollaberry <you@example.com>
2021-09-30 07:35:07 -0700

    function that return index of matching function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,20 +4,19 @@
  * @array: - an integer array
  * @size: size of the array
  * @cmp: function pointer
- * Return: -1 if element fail or < 0 or return index of element
+ * Return:index of element that matches function  or -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 if (size < 1 || array == NULL || cmp == NULL)
-{
 return (-1);
-}
 
 for (i = 0; size; i++)
 {
 if (cmp(array[i]))
 return (i);
 }
+
 return (-1);
 }
