6ceec2f6b2d0b9a7b065b7217d5f4943eb54180e
hollaberry <you@example.com>
2021-09-30 13:08:46 -0700

    fix betty style error

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - Struct op
  *
@@ -8,8 +8,8 @@
  */
 typedef struct op
 {
-  char *op;
-  int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
