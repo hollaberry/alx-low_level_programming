30b6c99ac5d23c68dfd610fd1e4657f87e5e7737
hollaberry <you@example.com>
2021-10-11 05:50:19 -0700

    function to free head

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * void free_list - function to free a list_t type of list
+ * @head - the list to be freed
+ */
+void free_list(list_t *head)
+{
+while (head != NULL)
+{
+if (head->str)
+{
+free(head->str);
+free(head);
+head = head->next;
+}
+}
