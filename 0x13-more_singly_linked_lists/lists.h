e47979e4449c47a8a57958ccb6d7975da2025a6a
hollaberry <you@example.com>
2021-10-13 08:01:59 -0700

    addded headers

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
