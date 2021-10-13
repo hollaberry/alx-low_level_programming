b9f3942020687163642299d848dc24b59638b603
hollaberry <you@example.com>
2021-10-13 13:31:56 -0700

    clear error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *temp, *new;
-unsigned int i = 1;
+unsigned int i;
 if (head == NULL)
 return (NULL);
 
@@ -19,6 +19,7 @@ temp = add_nodeint(head, n); /* call function to add node at the beginning */
 return (temp);
 }
 temp = *head; /* assign address of new value to temp */
+i = 1;
 while (i < idx)
 {
 temp = temp->next; /* tranverse through linked list until condition is false */
