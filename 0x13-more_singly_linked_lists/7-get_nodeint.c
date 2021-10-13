9780eeb5d6236f112c932258004845dabd0d9533
hollaberry <you@example.com>
2021-10-13 11:03:59 -0700

    func to return node at a given index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,21 +7,20 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *temp;
+
 unsigned int i = 0;
+
+while (i != index)
+{
 if (head == NULL)
 {
 return (NULL);
 }
 
-temp = head;
-while (temp->next != NULL)
-{
+head = head->next;
 i++;
-if (index == i)
-{
-return (temp);
 }
+return (head);
 }
  
  
