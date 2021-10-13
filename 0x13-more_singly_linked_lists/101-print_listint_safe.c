7d42c3af72f57a1c0b9a76dfc796a05b4a851ebd
hollaberry <you@example.com>
2021-10-13 14:34:04 -0700

    clear error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,19 +11,23 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-int i;
-
-i = 0;
-while (head != NULL)
+size_t print_listint_safe(const listint_t *head)
 {
-if ((head->next) >= (head))   /* if loop in list */
+size_t num = 0;
+long int diff;
+
+while (head)
+{
+diff = head - head->next;
+num++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (diff > 0)
+head = head->next;
+else
 {
 printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-return (i);
+break;
 }
-printf("[%p] %d\n", (void *)head, head->n);
-i++;
-head = head->next;
 }
-exit(98);
+return (num);
 }
