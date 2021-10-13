67ce2317d57d2d29b83641e4c1672f9c8b691896
hollaberry <you@example.com>
2021-10-13 08:59:20 -0700

    remove prototype error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,10 +7,9 @@
  */
 
 
-listint_t *add_nodeint_end(listint_t **head, const int *n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new, *temp;
-
 
 new = malloc(sizeof(listint_t));
 if (new == NULL)
@@ -22,9 +21,8 @@ new->n = n;
 new->next = NULL;
 
 if (*head == NULL)
-{
 *head = new;
-}
+
 else
 {
 temp = *head;
