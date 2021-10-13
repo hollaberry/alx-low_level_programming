b822a09a5d81581c002c31b6eb627f37841f0817
hollaberry <you@example.com>
2021-10-13 14:00:48 -0700

    correct function erors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,3 +41,22 @@ return (new);
       
       
       
+/**
+ *add_nodeint - adds a new node at the beginning of a listint_t list.
+ *@head : pointer to head node
+ *@n: value of the first member of the structure
+ *
+ *Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
+}
