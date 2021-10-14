f0fe8357e34c1a77aef0517c13b42732b5276e3e
hollaberry <you@example.com>
2021-10-14 14:06:28 -0700

    function to reset bit at index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit - reset the value of a bit at a given index.
+ * @n: integer n
+ * @index:  index, starting from 0 of the bit you want to get
+ *
+ *Return: value of the bit at index index or -1 if an error occured
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > (sizeof(n) * 8))
+return (-1);
+
+*n &= ~(1 << index);
+return (1);
+}
