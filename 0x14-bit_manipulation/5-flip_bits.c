6140796bbb051e78afef4bc9cb856c1723476f39
hollaberry <you@example.com>
2021-10-14 14:24:40 -0700

    function to count numbers of flipped bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * countset_bit - returns the number of bits to be flipped.
+ * @n: integer n
+ * Return: number of bits flipped
+ */
+
+int countset_bit(unsigned long int n)
+{
+unsigned long int count  = 0;
+while (n)
+{
+count += n & 1;
+n >>= 1;
+}
+return count;
+}
+
+/**
+ * int flip_bits - function to flip bits
+ * @n: first binary value
+ * @m: second binary value
+ * Return: number of bits flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+return countset_bits(n^m);
+}
+
