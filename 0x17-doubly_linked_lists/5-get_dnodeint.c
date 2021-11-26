9331fe16b9b513202aab52fb5332c5f888392e18
hollaberry <you@example.com>
2021-11-26 13:02:05 -0800

    nth node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function that returns the nth node of a linked list
+ * @head: holder of new data
+ * @index: index of value to be printed
+ * Return: value at index or null if node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+unsigned int i = 0;
+
+while (i != index)
+{
+if (head == NULL)
+{
+return (NULL);
+}
+
+head = head->next;
+i++;
+}
+return (head);
+}
