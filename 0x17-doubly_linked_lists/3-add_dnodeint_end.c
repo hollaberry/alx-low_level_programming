1caf12ed0d6f0338b0c6ac6859b1c9cd8f04cffa
hollaberry <you@example.com>
2021-11-26 12:16:15 -0800

    end lint

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * add_node_end - function that adds a node at the begining of a list
+ * @head: pointer to the first element of a node
+ * @n: string pointer
+ * Return: address of new element or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+
+dlistint_t *new, *temp;
+temp = *head;
+
+if (head == NULL)
+return (NULL);
+
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+
+new->n = n;
+
+new->next = NULL;
+new->prev = 
+if (*head == NULL)
+{
+*head = new;
+new->prev = NULL;
+return (new);
+}
+
+{
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+
+if (temp->next == NULL)
+{
+temp->next  = new;
+new->prev = temp;
+new->next = NULL;
+}
+return (new);
+}
