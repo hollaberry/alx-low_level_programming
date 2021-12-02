a1db80206801f896c5ff394d1e35a74cc29ef3cf
hollaberry <you@example.com>
2021-12-02 05:03:33 -0800

    operation dynamic

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,22 @@
+#include "operations.h"
+
+int add(int a, int b)
+{
+return (a + b);
+}
+int sum(int a, int b)
+{
+return (a + b);
+}
+int mul(int a, int b)
+{
+return (a * b);
+}
+int div(int a, int b)
+{
+return (a / b);
+}
+int mod(int a, int b)
+{
+return (a % b);
+}
